39230155cc0d87fe6fce52efc033639ebd2c3fe9
thelazybastard <256585194+thelazybastard@users.noreply.github.com>
2026-02-14 23:24:01 +0700

    added div and buttons and header
    div for storing tasks
    buttons for removing and adding tasks
    drop down for filter
    header and title

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "./index.html",
-    "./*.html"
-  ],
+  content: ["./src/**/*.{html,js}", "./*.html"],
   theme: {
     extend: {},
   },
